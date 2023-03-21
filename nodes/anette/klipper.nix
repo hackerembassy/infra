36f5d833a7dc780d510c5be7f0df44b834c9e484
Kei Moger <keimoger@gmail.com>
2023-03-21 23:26:53 +0400

    Added webcam

--- nodes/anette/klipper.nix
+++ nodes/anette/klipper.nix
@@ -53,9 +53,12 @@ in
       };
   };
   services.fluidd = on // {
-    nginx.extraConfig = ''
-      client_max_body_size 1G;
-    '';
+    nginx = {
+      extraConfig = ''
+        client_max_body_size 1G;
+      '';
+      locations."/webcam".proxyPass = "http://127.0.0.1:8080/stream";
+    };
   };
 
 }
