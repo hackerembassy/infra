5e6d2bfdb5c0777f03f578b554df31ce48bf0f02
Kei Moger <keimoger@gmail.com>
2023-04-28 01:34:52 +0400

    type mismatch fix

--- nodes/anette/klipper.nix
+++ nodes/anette/klipper.nix
@@ -69,6 +69,6 @@ in
   systemd.services.klippercam = {
     script = "${pkgs.ustreamer}/bin/ustreamer -f 15 -s 0"; 
     enable = true;
-    after = "network.target";
+    after = [ "network.target" ];
   };
 }
