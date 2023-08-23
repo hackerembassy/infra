4858e55d8ec33774796547ba8896ac9967bc1ff0
Vladimir Serov <me@cab404.ru>
2023-08-24 03:21:05 +0400

    klipperstuffs

--- nodes/le-succ/klipper.nix
+++ nodes/le-succ/klipper.nix
@@ -1,19 +1,22 @@
-{ config, pkgs, prelude, ... }@args: let
+{ config, pkgs, prelude, ... }@args:
+let
   on = { enable = true; };
 in
 
 # Klipper and stuff around it.
 
-  {
-    users.users.klipper = {
+{
+  imports = [ ./klipperscreen.nix ];
+
+  users.users.klipper = {
     isSystemUser = true;
     group = "klipper";
   };
 
-  users.groups.klipper = {};
+  users.groups.klipper = { };
 
   security.polkit = on;
-  
+
   services.klipper = on // {
     user = "klipper";
     group = "klipper";
@@ -33,24 +36,24 @@ in
     address = "0.0.0.0";
     allowSystemControl = true;
     settings = {
-        authorization = {
-          cors_domains = [
-            "http://${config.networking.hostName}"
-            "http://${config.networking.hostName}.lan"
-            "http://${config.networking.hostName}.local"
-            "http://localhost"
-            "http://app.fluidd.xyz"
-            "http://my.mainsail.xyz"
-      	    "http://le-fail.lan"
-      	    "http://printer-anette.lan"
-          ];
-          trusted_clients = [ "0.0.0.0/0" "::0/0" ];
-        };
-        file_manager = {
-          enable_object_processing = "False";
-        };
-        octoprint_compat = { };
+      authorization = {
+        cors_domains = [
+          "http://${config.networking.hostName}"
+          "http://${config.networking.hostName}.lan"
+          "http://${config.networking.hostName}.local"
+          "http://localhost"
+          "http://app.fluidd.xyz"
+          "http://my.mainsail.xyz"
+          "http://le-fail.lan"
+          "http://printer-anette.lan"
+        ];
+        trusted_clients = [ "0.0.0.0/0" "::0/0" ];
       };
+      file_manager = {
+        enable_object_processing = "False";
+      };
+      octoprint_compat = { };
+    };
   };
   services.fluidd = on // {
     nginx = {
@@ -62,36 +65,9 @@ in
   };
 
   systemd.services.klippercam = {
-    script = "${pkgs.ustreamer}/bin/ustreamer -f 15 -s 0"; 
+    script = "${pkgs.ustreamer}/bin/ustreamer -f 15 -s 0";
     enable = true;
     after = [ "network.target" ];
   };
 
-  systemd.services.klipperscreen = let 
-    iface = "enp0s20u5";
-    conf = builtins.toFile "KlipperConfig.conf" ''
-      [printer Anette]
-      moonraker_host: localhost
-      moonraker_port: 7125
-      
-      [printer Plumbus]
-      moonraker_host: printer-plumbus.lan
-      moonraker_port: 7125 
-    '';
-  in {
-    path = with pkgs; [ klipperscreen iproute2 ];
-    script = ''
-      export DISPLAY=$(ip r show dev ${iface} default | cut -d ' ' -f3):0
-      echo Starting at $DISPLAY
-      KlipperScreen -c ${conf}
-    ''; 
-    enable = true;
-    after = [ "moonraker.service" "sys-subsystem-nE${iface}t-devices-${iface}.device" ];
-    bindsTo = [ "sys-subsystem-net-devices-${iface}.device" ];
-    partOf = [ "sys-subsystem-net-devices-${iface}.device" ];
-    requires = [ "sys-subsystem-net-devices-${iface}.device" ];
-    wantedBy = [ "sys-subsystem-net-devices-${iface}.device" ];
-    serviceConfig.Restart = "always";
-  };
-  
 }
---
+++ nodes/le-succ/klipperscreen.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, prelude, ... }@args:
+let
+  on = { enable = true; };
+  iface = "enp0s20u5";
+  conf = builtins.toFile "KlipperConfig.conf" ''
+    [printer Anette]
+    moonraker_host: localhost
+    moonraker_port: 7125
+    
+    [printer Plumbus]
+    moonraker_host: printer-plumbus.lan
+    moonraker_port: 7125 
+  '';
+in
+
+# Klipper and stuff around it.
+
+{
+  systemd.services.klipperscreen = {
+    path = with pkgs; [ klipperscreen iproute2 ];
+    script = ''
+      export DISPLAY=$(ip r show dev ${iface} default | cut -d ' ' -f3):0
+      echo Starting at $DISPLAY
+      KlipperScreen -c ${conf}
+    '';
+    enable = true;
+    after = [ "moonraker.service" "sys-subsystem-net-devices-${iface}.device" ];
+    bindsTo = [ "sys-subsystem-net-devices-${iface}.device" ];
+    partOf = [ "sys-subsystem-net-devices-${iface}.device" ];
+    requires = [ "sys-subsystem-net-devices-${iface}.device" ];
+    wantedBy = [ "sys-subsystem-net-devices-${iface}.device" ];
+    serviceConfig.Restart = "always";
+  };
+
+  # systemd.services.klipperscreen-out = {
+  #   path = with pkgs; [ klipperscreen iproute2 ];
+  #   script = ''
+  #     export DISPLAY=klipperphone.lan:0
+  #     echo Starting at $DISPLAY
+  #     KlipperScreen -c ${conf}
+  #   '';
+  #   enable = true;
+  #   conflicts = [ "sys-subsystem-net-devices-${iface}.device" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig.Restart = "always";
+  # };
+}
