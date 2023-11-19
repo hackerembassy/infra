5fed66e75d541124376a41d3568d314d51a8c1f9
Vladimir Serov <cab+git@cab.moe>
2023-11-20 00:37:36 +0400

    headscale: enable ssh, fix killing times, placeholder acl

--- nodes/cumshot/configuration.nix
+++ nodes/cumshot/configuration.nix
@@ -12,7 +12,32 @@
 
   networking.networkmanager.enable = true;
   services.tailscale.enable = true;
-  services.headscale = {
+
+  systemd.services.headscale = {
+    # It is dumb-ish
+    serviceConfig.TimeoutStopSec = 2;
+    environment = {
+      HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1";
+    };
+  };
+  
+  services.headscale = let 
+    aclFile = builtins.toFile "acl.json" acl;
+    accept = src: dst: { action = "accept"; inherit src dst; }; 
+    deny = src: dst: { action = "deny"; inherit src dst; }; 
+    acl = builtins.toJSON {
+        acls = [
+          (accept ["*"] ["*:*"])
+        ];
+        ssh = [
+          (accept ["*"] ["sys"] // {users = ["*"];}) 
+          # (deny ["sys"] ["*"] // {users = ["*"];}) # IDK, it just denies *→sys as well :\
+        ];
+        autoApprovers = {
+          exitNode = [ "*" ];
+        };
+    };  
+  in {
     enable = true;
     settings = {
       server_url = "https://tail.hackem.cc";
@@ -21,6 +46,8 @@
         "100.64.0.0/16"
         "fd7a:115c:a1e0::/48"
       ];
+      log.level = "debug";
+      acl_policy_path = aclFile;
       dns_config = {
         magic_dns = true;
         nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
