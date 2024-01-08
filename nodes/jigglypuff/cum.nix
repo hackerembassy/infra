a873060701afe28d6fde7c3dba8d5d1cfc056752
Augustina DiPierro <tina@dipier.ro>
2024-01-08 05:30:09 +0400

    Deleted H-Ass, improved cum

--- nodes/jigglypuff/cum.nix
+++ nodes/jigglypuff/cum.nix
@@ -4,12 +4,15 @@
     enable = true;
     extraPackages = with pkgs; [ intel-media-driver vaapiIntel ];
   };
+
   services.go2rtc = {
-    enable = false;
+    enable = true;
+    settings = {
+      streams = {
+        cam = "ffmpeg:device?video=/dev/video0&input_format=yuyv422&video_size=640x480#video=h264#hardware=vaapi";
+      };
+      ffmpeg.bin = "${pkgs.ffmpeg_6-full}/bin/ffmpeg";
+    };
   };
-  # systemd.services.jigglycum = {
-  #   script = "${pkgs.ustreamer}/bin/ustreamer -f 15 -s 0 -p 7000";
-  #   enable = true;
-  #   after = [ "network.target" ];
-  # };
+
 }
