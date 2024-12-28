c1d54f3e2068da916dec85a74cd8389bf9c00694
Avgustina DiPierro <tina@dipier.ro>
2024-12-29 00:16:09 +0400

    headscale upgrade + oidc config

--- nodes/cumshot/tailscale.nix
+++ nodes/cumshot/tailscale.nix
@@ -3,28 +3,13 @@
   services.tailscale.enable = true;
   systemd.services.headscale = {
     # It is dumb-ish
-    serviceConfig.TimeoutStopSec = 2;
+    serviceConfig.TimeoutStopSec = 10;
     environment = {
       HEADSCALE_EXPERIMENTAL_FEATURE_SSH = "1";
     };
   };
   
   services.headscale = let 
-    aclFile = builtins.toFile "acl.json" acl;
-    accept = src: dst: { action = "accept"; inherit src dst; }; 
-    deny = src: dst: { action = "deny"; inherit src dst; }; 
-    acl = builtins.toJSON {
-        acls = [
-          (accept ["*"] ["*:*"])
-        ];
-        ssh = [
-          (accept ["*"] ["sys"] // {users = ["*"];}) 
-          # (deny ["sys"] ["*"] // {users = ["*"];}) # IDK, it just denies *→sys as well :\
-        ];
-        autoApprovers = {
-          exitNode = [ "*" ];
-        };
-    };  
   in {
     enable = true;
     settings = {
@@ -34,16 +19,27 @@
         "100.64.0.0/16"
         "fd7a:115c:a1e0::/48"
       ];
-      log.level = "debug";
-      acl_policy_path = aclFile;
-      dns_config = {
+      log.level = "info";
+      policy.mode = "database";
+      oidc = {
+	client_id = "x6Ji5XAWjYIl93mA4YBGvTpPRKc8Aw53CxYq6FPo";
+	client_secret_path = "/secrets/headscale-oidc-secret";
+	strip_email_domain = true;
+	scope = [
+	  "openid"
+	  "profile"
+	  "email"	
+	];
+	issuer = "https://a.hackem.cc/application/o/tailscale/";
+      };
+      dns = {
         magic_dns = true;
-        nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
-        base_domain = "hackem";
-        domains = [
+        #nameservers.global = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+        base_domain = "xkem.am";
+        search_domains = [
           "lan"
         ];
-        restricted_nameservers = {
+        nameservers.split = {
           "lan" = [
             "192.168.1.1"
           ];
