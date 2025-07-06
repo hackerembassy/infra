d1978594ad19393c128c291636216fc4f064bf38
Avgustina DiPierro <tina@dipier.ro>
2025-07-06 19:59:19 +0400

    chromium in app mode

--- nodes/telepoo/tv.nix
+++ nodes/telepoo/tv.nix
@@ -6,7 +6,7 @@
     environment = {
       NIXOS_OZONE_WL = "1";
     };
-    program = "${pkgs.chromium}/bin/chromium --kiosk --autoplay-policy=no-user-gesture-required --noerrdialogs --enable-features=OverlayScrollbar --disable-restore-session-state --disable-infobars --no-first-run https://ha.hackem.cc/phone-dash/0?BrowserID=inside-display";
+    program = "${pkgs.chromium}/bin/chromium --kiosk --autoplay-policy=no-user-gesture-required --noerrdialogs --enable-features=OverlayScrollbar --disable-restore-session-state --disable-infobars --no-first-run -app=https://ha.hackem.cc/phone-dash/0?BrowserID=inside-display";
   };
   systemd.services.cage-tty1.serviceConfig.Restart = "always";
 }
