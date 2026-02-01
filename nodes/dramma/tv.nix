a8a706cbbeab2e69ee4940489e5defb311aaf81e
Avgustina DiPierro <tina@dipier.ro>
2026-02-02 03:21:21 +0400

    chromium shall start only after network is online

--- nodes/dramma/tv.nix
+++ nodes/dramma/tv.nix
@@ -9,4 +9,5 @@
     program = "${pkgs.chromium}/bin/chromium --enable-native-gpu-memory-buffers --ozone-platform-hint=auto --enable-features=AcceleratedVideoEncoder,VaapiOnNvidiaGPUs,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE --enable-features=VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport --enable-features=UseMultiPlaneFormatForHardwareVideo --ignore-gpu-blocklist --enable-zero-copy --kiosk --autoplay-policy=no-user-gesture-required --noerrdialogs --enable-features=OverlayScrollbar --disable-restore-session-state --disable-infobars --no-first-run -app=https://ha.hackem.cc/phone-dash/0?BrowserID=inside-display";
   };
   systemd.services.cage-tty1.serviceConfig.Restart = "always";
+  systemd.services.cage-tty1.after = [ "network-online.target" ];
 }
